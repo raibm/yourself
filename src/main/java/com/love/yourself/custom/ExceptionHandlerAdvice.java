6e713fcd663f26111fd1b10f85418374e48696c6
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2021-11-15 18:04:22 -0300

    202111151743 - Adding a NoSuchElementException

---
+++ src/main/java/com/love/yourself/custom/ExceptionHandlerAdvice.java
@@ -0,0 +1,15 @@
+package com.love.yourself.custom;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public Object genericException(Object e) {
+        return e;
+    }
+}
